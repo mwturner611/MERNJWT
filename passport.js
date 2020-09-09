1db5bb696613cdc63d7b3ce52d2b88b530ad7dc4
mwturner611 <mwturner611@gmail.com>
2020-09-09 17:47:24 -0500

    passport.js

---
+++ passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
+
+passport.use(new LocalStrategy((username,password,done)=>{
+    User.findOne({username},(err,user)=>{
+        //something went wrong with database
+        if(err)
+            return done(err);
+
+        //if no user exists
+        if(!user)
+            return done(null,false);
+        //check if password is correct
+        user.comparePassword(password,done);
+    });
+}));
