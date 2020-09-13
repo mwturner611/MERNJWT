6a4f8c2db3b3be72809e2e6e8bffe3814a7ca852
mwturner611 <mwturner611@gmail.com>
2020-09-13 17:36:40 -0500

    Get todos route

--- routes/User.js
+++ routes/User.js
@@ -65,5 +65,15 @@ userRouter.post('/todo',passport.authenticate('jwt',{session: false}),(req,res)=
     })
 });
 
+userRouter.get('/todos',passport.authenticate('jwt',{session: false}),(req,res)=>{
+    User.findById({_id : req.user._id}).populate('todos').exec((err,document)=>{
+        if(err)
+            res.status(500).json({message: {msgBody: "Error has occured", msgError: true}})
+        else{
+            res.status(200).json({todos : document.todos, authenticated : true});
+        } 
+    })
+});
+
 
 module.exports = userRouter;
