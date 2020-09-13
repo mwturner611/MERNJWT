b013c55619949f4aab5479c729f0add6fb8f69f8
mwturner611 <mwturner611@gmail.com>
2020-09-13 18:42:13 -0500

    provided default user info to react from server

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AuthProvider from './Context/AuthContext'
 
-ReactDOM.render(<App />,document.getElementById('root')
+ReactDOM.render(
+<AuthProvider>
+    <App />
+</AuthProvider>,
+
+document.getElementById('root')
+
 );
 
 
