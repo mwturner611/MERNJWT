183c6794ee6efc97950261c5c6d8cdfe21dfb217
mwturner611 <mwturner611@gmail.com>
2020-09-13 17:45:06 -0500

    Admin get route

--- routes/User.js
+++ routes/User.js
@@ -75,5 +75,13 @@ userRouter.get('/todos',passport.authenticate('jwt',{session: false}),(req,res)=
     })
 });
 
+userRouter.get('/admin',passport.authenticate('jwt',{session: false}),(req,res)=>{
+    if(req.user.role === 'admin'){
+        res.status(200).json({message : {msgBody : 'You are an admin', msgError : false}});
+    }
+    else
+        res.status(403).json({message : {msgBody : "You're not an admin", msgError: true}});
+});
+
 
 module.exports = userRouter;
